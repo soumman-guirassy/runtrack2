b14b9568e5ac6ae0a776be15bbfeb79343f0eddd
Soumman-guirassy <guirassy.soumman@gmail.com>
2025-08-25 14:48:27 +0200

    Delete jour 3 directory

--- jour 3/job03/Untitled-1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
-
-// Parcours de la chaine
-for ($i = 0; $i < strlen($str); $i += 2) {
-    echo $str[$i];
-}
-?>
